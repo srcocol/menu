ec3e5b4ab7aef1af38d53689483b868ac8ea1ed8
Carlos Alvarez <79228981+srcocol@users.noreply.github.com>
2024-08-14 19:31:08 -0600

    Cambios Imagenes Sr Cocol

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ fetch('http://localhost:8000')
     .catch(error => {
         console.error('Error al obtener datos del backend:', error);
     });
+
+// script.js
+// Manejar el lightbox solo para imágenes en el menú
+document.querySelectorAll('main img').forEach(image => {
+  image.addEventListener('click', function() {
+      const lightbox = document.getElementById('lightbox');
+      const lightboxImg = document.getElementById('lightbox-img');
+      
+      // Obtener la ruta de la imagen clicada
+      const imgSrc = this.src;
+
+      // Reemplazar el nombre del archivo con la versión XL
+      const imgXL = imgSrc.replace('.jpg', 'XL.jpg');
+
+      // Mostrar el lightbox con la imagen XL
+      lightbox.style.display = "flex";  
+      lightboxImg.src = imgXL;  
+  });
+});
+
+// Cerrar el lightbox al hacer clic fuera de la imagen
+document.getElementById('lightbox').addEventListener('click', function(event) {
+  if (event.target !== document.getElementById('lightbox-img')) {
+      this.style.display = "none";
+  }
+});
